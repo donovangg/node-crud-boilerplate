31f8d8cbd1886b102e4f6ebc019d52cf8d12ee91
Donovan Gomez <DonovanG@ivl.ivleagueinc.com>
2025-10-03 14:02:04 -0700

    add CRUD functionality

--- server.js
+++ server.js
@@ -8,7 +8,32 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // read database (our db.json)
+function readDB() {
+  return JSON.parse(fs.readFileSync("db.json", "utf8"));
+}
 
+// Write to db 
+function writeDB(data) {
+  fs.writeFileSync("db.json", JSON.stringify(data, null, 2));
+}
+
+// GET all submissions
+app.get("/submissions", (req, res) => {
+  res.json(readDB());
+});
+
+// POST new submission
+app.post("/submissions", (req, res) => {
+  const db = readDB();
+  const newSubmission = {
+    id: Date.now(),
+    code: req.body.code,
+    name: req.body.name || "Anonymous"
+  };
+  db.push(newSubmission);
+  writeDB(db);
+  res.json(newSubmission);
+});
 
 
 // server uptime
