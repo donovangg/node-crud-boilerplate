febd31763914fff4f4031938b18fd12dbae2a21d
Donovan Gomez <DonovanG@ivl.ivleagueinc.com>
2025-10-03 17:28:02 -0700

    add Readme

---
+++ README.md
@@ -0,0 +1,35 @@
+# Just some Node Crud App Boilerplate nahimsayin
+
+## Getting Started
+
+### 1. Download or Clone the Repository
+
+- Download ZIP and extract **or** clone with Git:
+
+```bash
+git clone <repo-url>
+cd <repo-folder>
+```
+
+### 2. Install Dependencies 
+```bash 
+npm install
+```
+
+After running `npm install`, this project will install the following packages:
+
+- [express](https://www.npmjs.com/package/express) – Web framework for handling routes and requests.
+- [nodemon](https://www.npmjs.com/package/nodemon)  – Automatically restarts the server when files change.
+
+### 3. Start the Server
+```bash 
+node server.js
+```
+or
+
+```bash
+nodemon server.js
+```
+
+### 4. Open the App in Your Browser
+- [Right here dawg](http://localhost:3000)
--- server.js
+++ server.js
@@ -6,29 +6,33 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.static("public"));
 
+// db.json 
 const DB_FILE = "db.json";
 
-// Ensure db.json exists
+// Checks if DB_File exists, if not it creates an empty array
 if (!fs.existsSync(DB_FILE)) {
   fs.writeFileSync(DB_FILE, JSON.stringify([]));
 }
 
-// Helper: load submissions
+// Helper function: Loads submissions from DB_File / db.json
+// reads the File turns JSON into Javascript
 function loadSubmissions() {
   return JSON.parse(fs.readFileSync(DB_FILE));
 }
 
-// Helper: save submissions
+// Helper function: saves submissions into db.json
+// converts our Javascript back to Json and writes it to the file
 function saveSubmissions(submissions) {
   fs.writeFileSync(DB_FILE, JSON.stringify(submissions, null, 2));
 }
 
-// GET all submissions
+// GET request to get all your submissions
+// once project is running you can visit http://localhost:3000/submissions and see the data
 app.get("/submissions", (req, res) => {
   res.json(loadSubmissions());
 });
 
-// POST new submission
+// POST request and pushes whats in the form into db.json
 app.post("/submissions", (req, res) => {
   const submissions = loadSubmissions();
   const submission = req.body; // expects { name, code }
